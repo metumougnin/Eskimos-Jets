4f3d681a90207d88fc4e65587790768dbe567066
Metu Mougnin <m.mougnin@gmail.com>
2018-02-02 04:00:02 +0100

    Ajout des impulsions sur les deux cotés

---
+++ Assets/Thruster.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Thruster: Applique une force opposée au deplacement du joueur si celui entre en collision
+/// avec l'objet
+/// </summary>
+public class Thruster : MonoBehaviour {
+
+	// Permet d'indiquer dans quelle direction la force doit etre appliquee
+	public bool leftForce = false;
+	public bool rightForce = false;
+
+	private bool flag = false;
+
+	private Player playerRef;
+
+	// Puissance de la force à appliquer
+	public int power = 20;
+
+
+	void Awake() {
+		playerRef = GameObject.FindObjectOfType<Player> ();
+	}
+
+
+	void FixedUpdate() {
+		if (flag) {
+			Debug.Log ("YESS");
+
+			// direction de la force à appliquer
+			Vector3 direction = Vector3.zero;
+			if (leftForce) {
+				direction = Vector3.left;
+			} else if (rightForce) {
+				direction = Vector3.right;
+			}
+
+			playerRef.GetComponent<Rigidbody> ().AddForce (direction * power, ForceMode.Impulse);
+
+			flag = false;
+		}
+
+	}
+
+	void OnCollisionEnter( Collision other ) {
+		Debug.Log ("PING");
+		if( other.gameObject.tag == "Player" ){
+			Debug.Log ("OKOK");
+
+			flag = true;
+		}
+	}
+}
